854d15549661b27842cda955ea86381a407aebb9
vinnidias <vinicius.3991@gmail.com>
2021-06-01 15:47:25 -0300

    iniciando componente de card preview

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import logo from './imgs/Logo.png';
 import './style.css';
+import CardPreView from './components/CardPreView'
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+
 
 function App() {
+  const pokeList = ["bulbasaur", "ivysaur", "venusaur", "charmander", "charmeleon", "charizard", "squirtle", "wartortle", "blastoise", "caterpie", "metapod", "butterfree", "weedle", "kakuna", "beedrill", "pidgey", "pidgeotto", "pidgeot", "rattata", "raticate"]
+
+  useEffect(()=>{
+    axios.get('https://pokeapi.co/api/v2/pokemon/')
+      .then(res=>{
+        const list = res.data.results
+        
+        list.map(item=>{
+          pokeList.push(item.name)
+        })
+        console.log(pokeList)
+      })
+  },[pokeList])
+
   return (
     <div>
       <header className="header-container">
-        <img src={logo} className="logo-img"/>
+        <img src={logo} className="logo-img" />
       </header>
+      <div>
+
+      </div>
       <div className="cards-container">
-        
+        {
+          pokeList.map(item => (
+            <CardPreView id={item}/>
+          ))
+        }
       </div>
     </div>
   );
--- src/components/CardPreView/index.js
+++ src/components/CardPreView/index.js
@@ -1,11 +1,37 @@
-import style from './style.css'
+import { useEffect, useState } from 'react'
+import './style.css'
+import axios from 'axios'
 
+function getBackgroundFromType(type) {
+    if(type === 'grass'){
+        const grass = rgba(50, 224, 56, 1)
+        return grass
+    }
+    if(type === 'ghost'){
+        const ghost = rgba(177, 111, 218, 1)
+        return grass
+    }
+    if(type === 'ghost'){
+        const ghost = rgba(177, 111, 218, 1)
+        return grass
+    }
+}
 
-
-function CardPreView({img}) {
+export default function CardPreView({id}) {
+    const [img, setImg] = useState('')
+    const [background, setBackGround] = useState('')
+    
+    useEffect(()=>{
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        .then(res => {
+            const img = res.data.sprites.front_default
+            const type = res.data.types
+            setImg(img)
+        })
+    },[])
     return (
         <div className="card-preview">
-            <img src={img}/>
+            <img src={img} className="poke-img"/>
         </div>
     )
 }
