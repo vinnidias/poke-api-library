04a10b6375a40d04d29762fad29f2d7df9f4f70f
vinnidias <vinicius.3991@gmail.com>
2021-06-03 18:52:02 -0300

    inicio da lógica de busca

--- src/App.js
+++ src/App.js
@@ -2,35 +2,70 @@ import logo from './imgs/Logo.png';
 import './style.css';
 import CardPreView from './components/CardPreView'
 import { useEffect, useState } from 'react';
+import TextInput from '../src/components/TextInput'
 import axios from 'axios'
 
 
 function App() {
   const [pokeList, setList] = useState([])
-  
+  const [searchValue, setSearchValue] = useState('')
+  const [listView, setListView] = useState(true)
 
-  useEffect(()=>{
-    axios.get("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151")
-      .then(res=>{
+
+  const handleChange = (e) => {
+    const { value } = e.target
+    setSearchValue(value)
+
+  }
+
+  const handleClick = () => {
+    if(searchValue === '' && listView === true){
+      window.alert('No Pokémon found with that name!')
+    }else{
+      axios.get(`https://pokeapi.co/api/v2/pokemon/${searchValue}`)
+        .then(res => {
+          setListView(!listView)
+        })
+        .catch(e =>{
+          window.alert('No Pokémon found with that name!')
+          setSearchValue('')
+        })
+      
+    }
+    
+  }
+
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon/?offset=151&limit=302")
+      .then(res => {
         const list = res.data.results
         setList(list)
-        console.log('console da lista',pokeList)
       })
-  },[])
+  }, [])
 
   return (
     <div>
       <header className="header-container">
-        <img src={logo} className="logo-img" />
+        <img src={logo} className="logo-img" alt="Logo" />
       </header>
-      <div className="search-container">
-        
-      </div>
+      <TextInput searchValue={searchValue} handleChange={handleChange} handleClick={handleClick} />
+
       <div className="cards-container">
+
         {
-          pokeList.map(item => (
-            <CardPreView id={item.name}/>
-          ))
+          listView ?
+            pokeList.map(item => (
+              <CardPreView id={item.name} key={item.name} />
+            ))
+            :
+            <div style={{justifyContent: 'center'}}>
+              <button onClick={()=> {
+                setListView(true)
+              }}>
+                Back
+              </button>
+              <CardPreView id={searchValue} />
+            </div>
         }
       </div>
     </div>
